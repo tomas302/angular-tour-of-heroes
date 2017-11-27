7541cc2c76b6f316c7712c8a1cc03646220211fa
tomas dornell <tomasdornell@gmail.com>
2017-11-27 19:24:36 +0100

    Completed tutorial until 'Routing'.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
+    hero: Hero;
+  constructor(
+              private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location
+            ) {}
 
   ngOnInit() {
+      this.getHero();
   }
 
+    getHero(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.heroService.getHero(id)
+            .subscribe(hero => this.hero = hero);
+    }
+    
+    goBack(): void {
+        this.location.back();
+    }
 }
